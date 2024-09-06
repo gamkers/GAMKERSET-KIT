e99aca3bb4d930b871d7bc4ed685d147c2dd914e
AKASH <87299311+gamkers@users.noreply.github.com>
2024-09-06 11:27:50 +0530

    Update banner.py

--- modules/banner.py
+++ modules/banner.py
@@ -8,14 +8,6 @@ def banner():
       os.system("cls")
     else:
       os.system("clear")
-    print(Fore.LIGHTWHITE_EX+" (               )   (       *                (                     )       (")   
-    print(Fore.LIGHTWHITE_EX+" )\ )  *   )  ( /(   )\ )  (  `           (   )\ )       (       ( /(       )\ )")  
-    print(Fore.LIGHTWHITE_EX+"(()/(` )  /(  )\()) (()/(  )\))(        ( )\ (()/( (     )\      )\()) (   (()/( " )
-    print(Fore.LIGHTWHITE_EX+"/(_))( )(_))((_)\   /(_)_)()\       ___  )((_) /(_)))\ ((((_)(  |((_)\  )\   /(_)) ")
-    print(Fore.CYAN+" (_)) (_(_())   ((_) (_))  (_()((_)|___|((_)_ (_)) ((_) )\ _ )\ |_ ((_)((_) (_)) "  )
-    print(Fore.CYAN+"/ __||_   _|  / _ \ | _ \ |  \/  |      | _ )| _ \| __|(_)_\(_)| |/ / | __|| _ \ " )
-    print(Fore.CYAN+"\__ \  | |   | (_) ||   / | |\/| |      | _ \|   /| _|  / _ \    ' <  | _| |   / " )
-    print(Fore.CYAN+"|___/  |_|    \___/ |_|_\ |_|  |_|      |___/|_|_\|___|/_/ \_\  _|\_\ |___||_|_\\")
-    print(Style.RESET_ALL)
+    print(Fore.CYAN+" STARTING GAMKERSET-TOOL KIT")
 
 banner()
